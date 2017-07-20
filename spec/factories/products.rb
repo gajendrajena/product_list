c27fd357a474c13ae2b1fca820c1d8e887026219
Gajendra Jena <gjena@ctepl.com>
2017-07-20 11:34:32 +0530

    resolve conflicts

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,9 +1,5 @@
 FactoryGirl.define do
-<<<<<<< HEAD
   factory :product do
-=======
-  factory :product, class: Product do
->>>>>>> 29e992264dcfe0c226f08a6a3e9793869f66af17
     name "MyString"
     description "MyText"
     price 1.5
